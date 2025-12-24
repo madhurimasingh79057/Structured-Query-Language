2db108615f20accb5c0ac2c6990a2254f6b95ff6
Madhurima Singh <Madhurimajanta2007@gmail.com>
2025-12-24 10:58:32 +0530

    Add files via upload

---
+++ date functions.sql
@@ -0,0 +1,31 @@
+
+ #Date function
+ select current_date();
+ select curdate();
+ select sysdate();
+ select now();
+ select date("2025-10-09 11:59:53");
+ select time("2025-10-09 11:59:53");
+ select month("2025-10-09 11:59:53");
+ select year("2025-10-09 11:59:53");
+ select week("2025-10-09 11:59:53");
+ select weekday("2025-10-09 11:59:53");
+ select dayofweek("2025-10-09 11:59:53");
+ select dayofyear("2025-10-09 11:59:53");
+ select dayofmonth("2025-10-09 11:59:53");
+ select weekofyear("2025-10-09 11:59:53");
+ select monthname("2025-10-09 11:59:53");
+ select dayname("2025-10-09 11:59:53");
+ select last_day("2025-10-09 11:59:53");
+ select quarter("2025-10-09 11:59:53");
+ select yearweek("2025-10-09 11:59:53");
+ select weekday("2025-10-09 11:59:53");
+ 
+ #Extract
+ select extract(year from "2025-10-09 11:59:53");
+ select extract(month from "2025-10-09 11:59:53");
+ select extract(week from "2025-10-09 11:59:53");
+ select extract(day from "2025-10-09 11:59:53");
+ select extract(hour from "2025-10-09 11:59:53");
+ select extract(minute from "2025-10-09 11:59:53");
+ select extract(second from "2025-10-09 11:59:53");
